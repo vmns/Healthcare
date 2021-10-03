23ffe530f217ca5a37570202220ac80f98297a77
naga sekhar <sekharnaga939@gmail.com>
2021-10-03 11:47:17 +0530

    specialization repo update

---
+++ src/main/java/in/us/sekhar/hc/repo/SpecializationRepository.java
@@ -0,0 +1,9 @@
+package in.us.sekhar.hc.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.us.sekhar.hc.entity.Specialization;
+
+public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
+
+}
